a6f1128d262b50910b43f8cd7be72fe6b0ab742e
Dejan Vasic <dejanvasic24@gmail.com>
2026-04-02 22:59:30 +1100

    Initial commit and project setup with vite+ and tanstack start

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite-plus";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {
+    ignorePatterns: ["src/routeTree.gen.ts"],
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [
+    devtools(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
